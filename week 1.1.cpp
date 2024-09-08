766b48fae96e8812ab29d86f490a543429aa7e55
Sana5311 <sanamamnoon2580@gmail.com>
2024-09-08 21:04:24 +0530

    Add files via upload

---
+++ week 1.1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,sum;
+	printf("enter the first number:");
+	scanf("%d",&a);
+	printf("enter the second number:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("Sum = %d",sum);
+	return 0;
+}
---
+++ week 1.4.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    double num1, num2, result;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op); 
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Error! Operator is not correct.\n");
+            break;
+    }
+
+    return 0;
+}
